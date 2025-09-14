4b5d2e56abd9038e559a334a7e158d413065ed18
irfanMaitexa <irfan.maitexa@gmail.com>
2025-09-14 22:38:47 +0530

    html tage

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,6 +7,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <meta name="google-site-verification" content="DnwKJVa30q9to0ipyaN3x6yu1QHUbSZ3fRv4F5UpzCU" />
   <title>{{ $title }}</title>
   <script src="https://cdn.tailwindcss.com"></script>
 
