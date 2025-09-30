38c654f6f6fb497c4f0aa72335714362c9e161e7
MohammedIrfan33 <apirfan33@gmail.com>
2025-09-30 15:34:17 +0530

    route

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,6 +1,6 @@
 <nav class="bg-blue-700 text-white p-4 sticky top-0 z-50">
   <ul class="flex gap-6">
-    <x-navlink href="{{ route('dashboard') }}" :linkapage="request()->routeIs('home')">Home</x-navlink>
+    <x-navlink href="{{ route('dashboard') }}" :linkapage="request()->routeIs('dashboard')">Home</x-navlink>
     <x-navlink href="{{ route('course') }}" :linkapage="request()->routeIs('course')">Course</x-navlink>
     <x-navlink href="{{ route('payments') }}" :linkapage="request()->routeIs('payments')">Payments</x-navlink>
     <x-navlink href="{{ route('aboutus') }}" :linkapage="request()->routeIs('aboutus')">About</x-navlink>
